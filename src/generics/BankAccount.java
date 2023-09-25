16a2a67bc7757c837806addd05659f7f45ad0a00
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-25 14:47:43 +0100

    Implemented generic method

--- src/generics/BankAccount.java
+++ src/generics/BankAccount.java
@@ -1,5 +1,7 @@
 package generics;
 
+import java.util.Arrays;
+
 public class BankAccount<T> {
 
     T t1;
@@ -12,12 +14,25 @@ public class BankAccount<T> {
         System.out.println(t1);
     }
 
+    public <T> void depositMoney(T[] money) {
+        for (T i: money) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         BankAccount<String> b1 = new BankAccount<>("hello");
         BankAccount<Integer> b2 = new BankAccount<>(4);
 
-        b1.display();
-        b2.display();
+//        b1.display();
+//        b2.display();
+
+        Integer[] m1 = {100, 200, 300};
+        Double[] m2 = {20.45, 67.45, 30.00};
+
+        b1.depositMoney(m1);
+        b2.depositMoney(m2);
     }
 
 }
