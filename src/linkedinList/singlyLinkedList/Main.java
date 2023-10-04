468a69114e3528595dff6b68a6b43a2dd2a38283
Moses Hunsu <hunsumoses7@gmail.com>
2023-10-04 09:57:26 +0100

    Illustrated Singly Linked List

---
+++ src/linkedinList/singlyLinkedList/Main.java
@@ -0,0 +1,20 @@
+package linkedinList.singlyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList sLL = new SinglyLinkedList();
+        sLL.createSinglyLinkedList(5);
+//        System.out.println(sLL.head.value);
+        sLL.insertLinkedList(6, 1);
+        sLL.insertLinkedList(7, 3);
+        sLL.insertLinkedList(8, 4);
+        sLL.insertLinkedList(9, 0);
+        sLL.traversalSinglyLinkedList();
+//        sLL.searchNode(6);
+//        sLL.searchNode(17);
+        sLL.deleteSLL();
+        sLL.traversalSinglyLinkedList();
+//        System.out.println(sLL.head.next.next.value);
+
+    }
+}
---
+++ src/linkedinList/singlyLinkedList/Node.java
@@ -0,0 +1,6 @@
+package linkedinList.singlyLinkedList;
+
+public class Node {
+    public int value;
+    public Node next;
+}
